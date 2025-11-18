fd49d8040388db9b6c34fb3b285564dc655ef5e7
kpilszak <klaudia.pilszak@gmail.com>
2025-11-18 05:53:48 -0500

    Display images

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const API_KEY = "sk-"
 const submitIcon = document.querySelector("#submit-icon")
+const inputElement = document.querySelector("input")
+const imageSection = document.querySelector(".images-section")
 
-const getImages = () => {
+const getImages = async () => {
     const options = {
         method: "POST",
         headers: {
@@ -9,13 +11,22 @@ const getImages = () => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            "prompt": "",
+            "prompt": inputElement.value,
             "n": 4,
             "size": "1024x1024"
         })
     }
     try {
-        fetch('https://api.openai.com/v1/images/generations', options)
+        const response = await fetch('https://api.openai.com/v1/images/generations', options)
+        const data = await response.json()
+        data?.data.array.forEach(imageObject => {
+            const imageContainer = document.createElement('div')
+            imageContainer.classList.add('image-container')
+            const imageElement = document.createElement('img')
+            imageElement.setAttribute('src', imageObject.url)
+            imageContainer.append(imageElement)
+            imageSection.append(imageContainer)
+        })
     } catch (error) {
         console.error(error)
     }
